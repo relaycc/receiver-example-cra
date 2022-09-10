d677bd7b4daa8bd7cc4c45ae2d36930bffca8880
achilles schmelzer <achilles@daopanel.com>
2022-09-10 15:17:33 -0700

    Add dynamic addresses example
    * Add react-router (because dyn addresses is another page)
    * Remove the default create-react-app stuff (like the logo asset)
    * Add some basic button styling
    * Add dynamic address examples at /dynamic page
    * Move the wagmi and rainbowkit config into index.tsx to make
      receiver-specific code a bit more obvious

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,71 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
-import {
-  Launcher,
-  Window,
-  useLaunch,
-  useSetWallet,
-  useIsOpen,
-} from "@relaycc/receiver";
-import {
-  ConnectButton,
-  RainbowKitProvider,
-  getDefaultWallets,
-} from "@rainbow-me/rainbowkit";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
-import {
-  WagmiConfig,
-  configureChains,
-  createClient,
-  defaultChains,
-  useSigner,
-} from "wagmi";
-import "@rainbow-me/rainbowkit/dist/index.css";
-import { Wallet } from "ethers";
-
-const devWallet =
-  process.env.NODE_ENV === "development"
-    ? new Wallet(process.env.REACT_APP_TEST_PK as string)
-    : undefined;
-
-const alchemyKey = "kmMb00nhQ0SWModX6lJLjXy_pVtiQnjx";
-
-const { chains, provider } = configureChains(defaultChains, [
-  alchemyProvider({ apiKey: alchemyKey }),
-  publicProvider(),
-]);
-
-const { connectors } = getDefaultWallets({
-  appName: "Relay Receiver Example App",
-  chains,
-});
-
-const wagmiClient = createClient({
-  autoConnect: false,
-  connectors,
-  provider,
-});
+import { Launcher, Window, useLaunch, useIsOpen } from "@relaycc/receiver";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<BasicExample />} />
+      <Route path="dynamic" element={<DynamicExample />} />
+    </Routes>
+  );
+}
+
+function BasicExample() {
   const launch = useLaunch();
   const isOpen = useIsOpen();
 
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-        <div className="full-flex-centered">
-          <h1 className="header">Relay Receiver Example App</h1>
-          <h1>First connect a wallet</h1>
-          <ConnectButton />
-          <h1>Then launch receiver</h1>
-          {isOpen || (
-            <button className="launch-receiver" onClick={() => launch()}>
-              Launch Receiver
-            </button>
-          )}
-          {isOpen && (
-            <div className="receiver-is-open">Receiver Window is Open</div>
-          )}
-          <h1>Then follow the instructions in the popup 🎉</h1>
-          <WalletExample />
-          <Window />
-          <Launcher />
-        </div>
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <div className="full-flex-centered">
+      <h1 className="header">Relay Receiver Example App</h1>
+      <h1>First connect a wallet</h1>
+      <ConnectButton />
+      <h1>Then launch receiver</h1>
+      {isOpen || (
+        <button
+          className="launch-receiver hover-scale"
+          onClick={() => launch()}
+        >
+          Launch Receiver
+        </button>
+      )}
+      {isOpen && (
+        <div className="receiver-is-open">Receiver Window is Open</div>
+      )}
+      <h1>Then follow the instructions in the popup 🎉</h1>
+      <Window />
+      <Launcher />
+    </div>
   );
 }
 
-function WalletExample() {
-  const { data: wallet } = useSigner();
-  const setWallet = useSetWallet();
+function DynamicExample() {
+  const launch = useLaunch();
 
-  useEffect(() => {
-    setWallet(devWallet || wallet || null);
-  }, [wallet, setWallet]);
-
-  return <></>;
+  return (
+    <div className="full-flex-centered">
+      <h1>You can turn any component into a Receiver Launcher</h1>
+      <ConnectButton />
+      <h1>and send the user straight into a conversation!</h1>
+      <button
+        className="launch-receiver hover-scale"
+        onClick={() => launch("0x0cb27e883E207905AD2A94F9B6eF0C7A99223C37")}
+      >
+        Talk to the Relay Founder
+      </button>
+      <h1>You can message anyone with an ETH account</h1>
+      <button
+        className="launch-receiver hover-scale"
+        onClick={() => launch("0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045")}
+      >
+        Even Vitalik!
+      </button>
+      <h2>(Once he's joined the network 🎉)</h2>
+      <Window />
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { useSetWallet } from "@relaycc/receiver";
+import {
+  WagmiConfig,
+  configureChains,
+  createClient,
+  defaultChains,
+  useSigner,
+} from "wagmi";
+import { Wallet } from "ethers";
+import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/dist/index.css";
+
+const alchemyKey = "kmMb00nhQ0SWModX6lJLjXy_pVtiQnjx";
+
+const { chains, provider } = configureChains(defaultChains, [
+  alchemyProvider({ apiKey: alchemyKey }),
+  publicProvider(),
+]);
+
+const { connectors } = getDefaultWallets({
+  appName: "Relay Receiver Example App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider,
+});
+
+const devWallet =
+  process.env.NODE_ENV === "development"
+    ? new Wallet(process.env.REACT_APP_TEST_PK as string)
+    : undefined;
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <App />
+          <SetWalletExample />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
+function SetWalletExample() {
+  const { data: wallet } = useSigner();
+  const setWallet = useSetWallet();
+  console.log(wallet);
+
+  useEffect(() => {
+    setWallet(devWallet || wallet || null);
+  }, [wallet, setWallet]);
+
+  return <></>;
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
