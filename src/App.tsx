a0d13a6f52c11a955c22dfb7c4a28ea766415974
achilles schmelzer <achilles@daopanel.com>
2022-09-09 17:30:09 -0700

    Bump @relaycc/receiver version and add some flair
    Made the app less of a blank create-react-app app and more of a
    barebones relay-receiver app. Added rainbowkit and a couple of
    very basic usage hints.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Receiver, FixedLaunch } from '@relaycc/receiver';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import {
+  Launcher,
+  Window,
+  useLaunch,
+  useSetWallet,
+  useIsOpen,
+} from "@relaycc/receiver";
+import {
+  ConnectButton,
+  RainbowKitProvider,
+  getDefaultWallets,
+} from "@rainbow-me/rainbowkit";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import {
+  WagmiConfig,
+  configureChains,
+  createClient,
+  defaultChains,
+  useSigner,
+} from "wagmi";
+import "@rainbow-me/rainbowkit/dist/index.css";
+const alchemyKey = "kmMb00nhQ0SWModX6lJLjXy_pVtiQnjx";
+
+const { chains, provider } = configureChains(defaultChains, [
+  alchemyProvider({ apiKey: alchemyKey }),
+  publicProvider(),
+]);
+
+const { connectors } = getDefaultWallets({
+  appName: "Relay Receiver Example App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider,
+});
 
 function App() {
+  const launch = useLaunch();
+  const isOpen = useIsOpen();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Receiver>
-          <FixedLaunch peerAddress="0x45C9a201e2937608905fEF17De9A67f25F9f98E0"/>
-        </Receiver>
-      </header>
-    </div>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <div className="full-flex-centered">
+          <h1 className="header">Relay Receiver Example App</h1>
+          <h1>First connect a wallet</h1>
+          <ConnectButton />
+          <h1>Then launch receiver</h1>
+          {isOpen || (
+            <button className="launch-receiver" onClick={() => launch()}>
+              Launch Receiver
+            </button>
+          )}
+          {isOpen && (
+            <div className="receiver-is-open">Receiver Window is Open</div>
+          )}
+          <h1>Then follow the instructions in the popup 🎉</h1>
+          <WalletExample />
+          <Window />
+          <Launcher />
+        </div>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
+}
+
+function WalletExample() {
+  const { data: wallet } = useSigner();
+  const setWallet = useSetWallet();
+
+  useEffect(() => {
+    setWallet(wallet || null);
+  }, [wallet, setWallet]);
+
+  return <></>;
 }
 
 export default App;
