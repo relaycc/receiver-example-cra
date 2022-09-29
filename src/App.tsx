8007776b6cf429379324a307aed7735db8ae9900
brian schafer <roycepdev@gmail.com>
2022-09-29 15:46:51 -0700

    added github link to all 3 pages

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import {
 } from "@relaycc/receiver";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Routes, Route, Link } from "react-router-dom";
+import github from "./github.svg";
 
 function App() {
   return (
@@ -29,25 +30,14 @@ function BasicExample() {
     <div className="full-flex-centered">
       {" "}
       <header className="header-with-link">
-        Relay Receiver is open source, check it out on
+        Relay Receiver is open source, check it out on test
         <a
           href="https://github.com/relaycc/receiver"
           target="_blank"
           rel="noreferrer"
-          style={{ display: "flex", alignItems: "center"}}
+          style={{ display: "flex", alignItems: "center" }}
         >
-          <svg
-            viewBox="0 0 24 24"
-            height={30}
-            width={30}
-            style={{ cursor: "pointer" }}
-          >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"
-            ></path>
-          </svg>
+          <img src={github} alt="Link to relay Github page" />
         </a>
       </header>
       <h1 className="header">Relay Receiver Example App</h1>
@@ -91,19 +81,9 @@ function DynamicExample() {
           href="https://github.com/relaycc/receiver"
           target="_blank"
           rel="noreferrer"
-          style={{ display: "flex", alignItems: "center"}}
+          style={{ display: "flex", alignItems: "center" }}
         >
-          <svg
-            viewBox="0 0 24 24"
-            height={30}
-            width={30}
-          >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"
-            ></path>
-          </svg>
+          <img src={github} alt="Link to relay github page" />
         </a>
       </header>
       <h1 className="header">Message Any Address!</h1>
@@ -144,6 +124,17 @@ function CustomExample() {
 
   return (
     <div className="custom">
+      <header className="header-with-link">
+        Relay Receiver is open source, check it out on test
+        <a
+          href="https://github.com/relaycc/receiver"
+          target="_blank"
+          rel="noreferrer"
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          <img src={github} alt="Link to relay github page" />
+        </a>
+      </header>
       <h1 className="header">Receiver is customizable!</h1>
       <h1>For example, you could use it as a modal.</h1>
       <button
